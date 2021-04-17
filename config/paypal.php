8e7d276ef9a546892be86a596066b6a191314cde
ahmednoaman12 <ahmednoamanhassan@gmail.com>
2021-04-17 23:07:37 +0200

    paypal

---
+++ config/paypal.php
@@ -0,0 +1,12 @@
+<?php 
+return [ 
+    'client_id' => 'AR1oeB7TAGxAECKRd3Qt6wCMrDUNSJDmAGKPsRnXLF2SzTaPp_TCF62zgf_yqSXn8VP16HPR6ls8Nt3F',
+	'secret' => 'EDVBL0R26MP8XnxPpiTwcUy0r5FswczPVaezfEOxfAoJ_--rYY2TGHBp5emJ1rK_YwO3WugsNvU816MB',
+    'settings' => array(
+        'mode' => 'sandbox',
+        'http.ConnectionTimeOut' => 1000,
+        'log.LogEnabled' => true,
+        'log.FileName' => storage_path() . '/logs/paypal.log',
+        'log.LogLevel' => 'FINE'
+    ),
+];
